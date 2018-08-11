9f244b28bb9c018c989bc45037e37b96878e5a1f
Feilena <42264844+Feilena@users.noreply.github.com>
2018-08-11 02:52:08 -0500

    #FarmingTime
    Change Log:
    
    Created Items/Blocks/Ore for the following:
    
    Magnesium
    Sodium
    Phosphorous
    Copper
    Manganese
    Boron
    Zinc
    Cobalt
    Selenium
    Molybdenum
    
    Updated for modularization.
    
    Committing for redundancy.

---
+++ src/main/java/com/feilena/own/block/MolybdenumOre.java
@@ -0,0 +1,20 @@
+package com.feilena.own.block;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class MolybdenumOre extends BlockBase {
+
+public MolybdenumOre(String name, Material material) {
+		super(name, material);
+		
+		setSoundType(SoundType.STONE);
+		setHardness(3.0F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		//setLightLevel(1.0F);
+		//setLightOpacity(1);
+		//setBlockUnbreakable();
+	}
+
+}
--- src/main/java/com/feilena/own/util/handlers/RegistryHandler.java
+++ src/main/java/com/feilena/own/util/handlers/RegistryHandler.java
@@ -1,8 +1,10 @@
 package com.feilena.own.util.handlers;
 
+import com.feilena.own.init.ModBlocks;
 import com.feilena.own.init.ModItems;
 import com.feilena.own.util.IHasModel;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -16,6 +18,14 @@ public class RegistryHandler {
 	public static void onItemRegister(RegistryEvent.Register<Item> event){
 		event.getRegistry().registerAll(ModItems.items.toArray(new Item[0]));
 	}
+	
+	
+	@SubscribeEvent
+	public static void onBlockRegister(RegistryEvent.Register<Block> event){
+		event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+	}
+	
+	
 	@SubscribeEvent
 	public static void onModelRegister(ModelRegistryEvent event){
 		for(Item item : ModItems.items){
@@ -23,5 +33,13 @@ public class RegistryHandler {
 				((IHasModel)item).registerModels();
 			}
 		}
+		
+		for(Block block : ModBlocks.BLOCKS){
+			if(block instanceof IHasModel){
+				((IHasModel)block).registerModels();
+			}
+		}
+		
 	}
+	
 }
